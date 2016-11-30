f9052f401bee530b16f32e9cad3e3b1f242e1428
AlejandroStevensonDuran <astevensond@utexas.edu>
2016-11-29 19:40:41 -0600

    Add files via upload

---
+++ src/day23network/observer/ClientObserver.java
@@ -0,0 +1,18 @@
+package day23network.observer;
+
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Observable;
+import java.util.Observer;
+
+public class ClientObserver extends PrintWriter implements Observer {
+	public ClientObserver(OutputStream out) {
+		super(out);
+	}
+	@Override
+	public void update(Observable o, Object arg) {
+		this.println(arg); //writer.println(arg);
+		this.flush(); //writer.flush();
+	}
+
+}
